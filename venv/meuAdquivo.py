9ac21b0d8fe53cedabe7d8244bc6f67fdb260853
Vinícius Meneguetti Boniatti <182320002@senacinformatica.br>
2025-01-17 17:16:20 -0300

    Aula Finalizada utilizando Pandas

--- venv/meuAdquivo.py
+++ venv/meuAdquivo.py
@@ -16,23 +16,36 @@ print(tabela_de_vendas)
 #faturamento da loja
 faturamento = tabela_de_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
 print(faturamento)
-print("=========================================")
+print("="*20)
 
 #quantidade de proputos
 print("QUANTIDADE")
+print("="*20)
 quantidade = tabela_de_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
 print(quantidade)
-print("=========================================")
+print("="*20)
 
 #tiket medio por loja
 
 print("TICKET MÉDIO")
-print("=========================================")
+print("="*20)
 ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
 print(ticket_medio)
 
 #enviar um email
+import win32com.client as win32
+
+outlook = win32.Dispatch('outlook.application')
+mail = outlook.createItem(0)
+mail.To = 'viniciusmboniatti@gmail.com'
+mail.Subject = 'Manssage Subject'
+mail.HTMLBody ='<h2> MENSAGEM QUE SERÁ ENVIADA </h2>'
+mail.send()
+
+# ele envia a mensagem pro email descrito em cima, usando o 
+# outlook enviando pra qual email, tem que estar logado no app 
+# do outlook no PC.
+ 
 
 
 
-#visualizar a base de dados
